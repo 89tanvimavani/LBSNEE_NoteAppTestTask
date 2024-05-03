1226b5723a2b6651d50334d8695eacad876f10e2
89tanvimavani <tanumavani89@gmail.com>
2024-05-03 13:52:52 +0530

    first commit

---
+++ src/screens/NoteScreen/components/noteScreen.js
@@ -0,0 +1,49 @@
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  SafeAreaView,
+  StatusBar,
+  Platform,
+} from 'react-native';
+import React, {useState} from 'react';
+import {styles} from '../styles/noteScreen.style';
+import {NoteListView} from './noteListView';
+import {AddNote} from './addNote';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import {Colors} from '../../../assets/colors';
+
+const NoteScreen = () => {
+  const [addNoteEnable, setAddNoteEnable] = useState(false);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* STATUS BAR */}
+      <StatusBar
+        translucent
+        backgroundColor={Colors.primaryColor}
+        barStyle={Platform.OS === 'ios' ? 'dark-content' : 'light-content'}
+      />
+
+      {/* HEADER */}
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Your Notes</Text>
+      </View>
+
+      {/* NOTE LISTS */}
+      <NoteListView />
+
+      {/* ADD NOTE COMPONENT */}
+      <AddNote visible={addNoteEnable} setVisible={setAddNoteEnable} />
+
+      {/* ADD NOTE FLOATING BUTTON */}
+      <TouchableOpacity
+        onPress={() => setAddNoteEnable(true)}
+        style={styles.floatingButton}>
+        <FontAwesome5 name="plus" color="white" size={20} />
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+export default NoteScreen;
